a92fc864d80711fab91bdf9f6e33052b0bc1954c
Igor Rodrigues <48895884+igorrodriguesss@users.noreply.github.com>
2022-02-24 22:35:19 -0300

    Update and rename main1.dart to main13.dart

--- main13.dart
+++ main13.dart
@@ -1,3 +1,12 @@
+/*Implemente um programa em Dart que leia o total de um objeto em estoque,
+o total vendido
+de objetos do mesmo tipo, calcule e mostre o percentual de objetos vendidos.
+Total de um Objeto em Estoque (TE) ----------- 100 %
+
+Total Vendido de Objetos (TV) ------------ X %
+X = TV * 100 / TE
+*/
+
 import 'dart:io';
 void main() {
 
@@ -9,7 +18,4 @@ void main() {
 
   var totalVendido = (qntVendida * 100)/qntArroz;
   print(totalVendido);
-  
-
-  
 }
