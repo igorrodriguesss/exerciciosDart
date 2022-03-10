72bde11265f4112883fd12fc243e22deca936447
Igor Rodrigues <48895884+igorrodriguesss@users.noreply.github.com>
2022-03-10 18:33:21 -0300

    Create main4.dart

---
+++ main4.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'dart:math';
+
+main() {
+
+	const double pi = 3.1415926535897932;
+
+	print("Digite valor do raio:");
+	double raio = double.parse(stdin.readLineSync() 
+ ?? '0');
+
+	double area = pi * (pow(raio, 2));
+	print(area.toStringAsFixed(2));
+
+}
