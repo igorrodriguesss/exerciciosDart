ca9160ad1b2dbb360b8b9a7bb8ab00f698624e9c
Igor Rodrigues <48895884+igorrodriguesss@users.noreply.github.com>
2022-03-31 21:34:42 -0300

    Create SPLIT.dart

---
+++ SPLIT.dart
@@ -0,0 +1,23 @@
+var linha = stdin.readLineSync() ?? "0.0 0.0 0.0";
+    var lista = linha.split(" ");
+
+    var pi = 3.14159;
+    
+    double A = double.parse(lista[0]);
+    double B = double.parse(lista[1]);
+    double C = double.parse(lista[2]);
+    
+    double TRIANGULO = (A*C)/2;
+    print('TRIANGULO: ' + TRIANGULO.toStringAsFixed(3));
+   
+    double CIRCULO = pi * C*C;
+    print('CIRCULO: ' + CIRCULO.toStringAsFixed(3));
+  
+    double TRAPEZIO = ((A+B)*C)/2;
+    print('TRAPEZIO: ' + TRAPEZIO.toStringAsFixed(3));
+
+  double QUADRADO = (B*B);
+  print('QUADRADO:' + QUADRADO.toStringAsFixed(3));
+
+  double RETANGULO = (A*B);
+  print('RETANGULO:' + RETANGULO.toStringAsFixed(3));
