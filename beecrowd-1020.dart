d9a558f393e180f4b4ba0261441f130ab8b54510
Igor Rodrigues <48895884+igorrodriguesss@users.noreply.github.com>
2022-04-04 11:17:28 -0300

    Create beecrowd-1020.dart

---
+++ beecrowd-1020.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main() {
+  
+  int idadeDias = int.parse(stdin.readLineSync() ?? "0");
+  
+  int anos = idadeDias ~/ 365;
+  
+  idadeDias -= anos * 365;
+  
+  int meses = idadeDias ~/ 30;
+  
+  idadeDias -= meses * 30;
+  
+  int dias = idadeDias;
+  
+  print("$anos ano(s)");
+  print("$meses mes(es)");
+  print("$dias dia(s)");
+}
